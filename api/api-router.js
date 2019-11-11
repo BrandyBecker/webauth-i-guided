4b9dbec26c5bb98c5ad91afba53046b67a870698
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-11-11 12:31:25 -0500

    k

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
@@ -9,5 +9,14 @@ router.use('/users', usersRouter);
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req,res)=>{
+  //read a password from the body, hash the password,
+  //then return to user in a obj that looks like 
+  // {password: 'original password', hash: 'hashed password'}
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  res.json({password: req.body.password, hash: hash})
+})
 
 module.exports = router;
